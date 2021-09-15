6001bd148f2b0340c37673c3e4de49fa5a4a1308
Anna Filuykova <ann_muyer111@mail.ru>
2021-09-15 17:09:34 +0300

    решение 1 модуля

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -1,6 +1,3 @@
-/**
- * Эту функцию трогать не нужно
- */
 function print(text) {
   console.log(text);
 }
@@ -10,7 +7,9 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  // ваш код...
+  if (!name || name.length < 4 || !!name.match(' ')) return false;
+
+  return true;
 }
 
 function sayHello() {
@@ -22,3 +21,5 @@ function sayHello() {
     print('Некорректное имя');
   }
 }
+
+sayHello();
