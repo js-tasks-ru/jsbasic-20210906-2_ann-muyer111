9e390e4e430db2cc88c75355907d7a7d1010ee9d
Anna Filuykova <ann_muyer111@mail.ru>
2021-10-07 16:34:00 +0300

    выполнение 5 модуля

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,11 @@
 function toggleText() {
-  // ваш код...
+  const button = document.querySelector('.toggle-text-button');
+  const text = document.querySelector('div');
+  button.addEventListener('click', () => {
+    if (text.hidden === false) {
+      text.hidden = true;
+    } else {
+      text.hidden = false;
+    }
+  });
 }
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,36 @@
 function initCarousel() {
-  // ваш код...
+  const buttonRight = document.querySelector('.carousel__arrow_right');
+  const buttonLeft = document.querySelector('.carousel__arrow_left');
+  const inner = document.querySelector('.carousel__inner');
+  const carouselImg = document.querySelector('.carousel__img');
+  const carouselImgWidth = carouselImg.offsetWidth;
+  let position = 0;
+
+  const checkPosition = () => {
+    if (position === 0) {
+      buttonLeft.classList.add('hidden');
+      buttonRight.classList.remove('hidden');
+    } else if (position === inner.children.length - 1) {
+      buttonLeft.classList.remove('hidden');
+      buttonRight.classList.add('hidden');
+    } else {
+      buttonLeft.classList.remove('hidden');
+      buttonRight.classList.remove('hidden');
+    }
+  };
+
+  checkPosition();
+
+  buttonRight.addEventListener('click', () => {
+    position++;
+    inner.style.transform = `translateX(-${position * carouselImgWidth}px)`;
+    checkPosition();
+  });
+
+  buttonLeft.addEventListener('click', () => {
+    console.log('!!!');
+    position--;
+    inner.style.transform = `translateX(-${position * carouselImgWidth}px)`;
+    checkPosition();
+  });
 }
